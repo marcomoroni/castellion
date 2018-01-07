001f7cf1408ae619d03adbce4c0afeda40af5d02
Marco Moroni <marcomoroni394@gmail.com>
2018-01-07 09:27:45 +0000

    Change some naming

--- castellion.py
+++ castellion.py
@@ -4,7 +4,7 @@ import random
 
 
 
-class TileFaction(Enum):
+class DreamTileFaction(Enum):
     SEER = 0
     CHAMELEON = 1
     JUGGLER = 2
@@ -12,14 +12,14 @@ class TileFaction(Enum):
 
 
 
-class TileShape(Enum):
+class DreamTileShape(Enum):
     SQUARE = 0
     CIRCLE = 1
     TRIANGLE = 2
 
 
 
-class Tile:
+class DreamTile:
 
     def __init__(self, faction, shape, safe=False):
         self.faction = faction
@@ -33,13 +33,13 @@ class Tile:
 
         # change text colour
         toPrint = toPrint + Fore.BLACK
-        if self.faction == TileFaction.SEER:
+        if self.faction == DreamTileFaction.SEER:
             toPrint = toPrint + Back.CYAN
-        elif self.faction == TileFaction.CHAMELEON:
+        elif self.faction == DreamTileFaction.CHAMELEON:
             toPrint = toPrint + Back.GREEN
-        elif self.faction == TileFaction.JUGGLER:
+        elif self.faction == DreamTileFaction.JUGGLER:
             toPrint = toPrint + Back.RED
-        elif self.faction == TileFaction.PYRO:
+        elif self.faction == DreamTileFaction.PYRO:
             toPrint = toPrint + Back.MAGENTA
 
         # safe tiles
@@ -47,11 +47,11 @@ class Tile:
             toPrint = toPrint + Fore.YELLOW
 
         # print shape
-        if self.shape == TileShape.SQUARE:
+        if self.shape == DreamTileShape.SQUARE:
             toPrint = toPrint + " A "
-        elif self.shape == TileShape.CIRCLE:
+        elif self.shape == DreamTileShape.CIRCLE:
             toPrint = toPrint + " B "
-        elif self.shape == TileShape.TRIANGLE:
+        elif self.shape == DreamTileShape.TRIANGLE:
             toPrint = toPrint + " C "
 
         # reset colour
@@ -65,91 +65,91 @@ mainPile = []
 safePile = []
 
 # create all main tiles
-newTile = Tile(TileFaction.SEER, TileShape.SQUARE)
+newTile = DreamTile(DreamTileFaction.SEER, DreamTileShape.SQUARE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
-newTile = Tile(TileFaction.SEER, TileShape.CIRCLE)
+newTile = DreamTile(DreamTileFaction.SEER, DreamTileShape.CIRCLE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
-newTile = Tile(TileFaction.SEER, TileShape.TRIANGLE)
+newTile = DreamTile(DreamTileFaction.SEER, DreamTileShape.TRIANGLE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
-newTile = Tile(TileFaction.CHAMELEON, TileShape.SQUARE)
+newTile = DreamTile(DreamTileFaction.CHAMELEON, DreamTileShape.SQUARE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
-newTile = Tile(TileFaction.CHAMELEON, TileShape.CIRCLE)
+newTile = DreamTile(DreamTileFaction.CHAMELEON, DreamTileShape.CIRCLE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
-newTile = Tile(TileFaction.CHAMELEON, TileShape.TRIANGLE)
+newTile = DreamTile(DreamTileFaction.CHAMELEON, DreamTileShape.TRIANGLE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
-newTile = Tile(TileFaction.JUGGLER, TileShape.SQUARE)
+newTile = DreamTile(DreamTileFaction.JUGGLER, DreamTileShape.SQUARE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
-newTile = Tile(TileFaction.JUGGLER, TileShape.CIRCLE)
+newTile = DreamTile(DreamTileFaction.JUGGLER, DreamTileShape.CIRCLE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
-newTile = Tile(TileFaction.JUGGLER, TileShape.TRIANGLE)
+newTile = DreamTile(DreamTileFaction.JUGGLER, DreamTileShape.TRIANGLE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
-newTile = Tile(TileFaction.PYRO, TileShape.SQUARE)
+newTile = DreamTile(DreamTileFaction.PYRO, DreamTileShape.SQUARE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
-newTile = Tile(TileFaction.PYRO, TileShape.CIRCLE)
+newTile = DreamTile(DreamTileFaction.PYRO, DreamTileShape.CIRCLE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
-newTile = Tile(TileFaction.PYRO, TileShape.TRIANGLE)
+newTile = DreamTile(DreamTileFaction.PYRO, DreamTileShape.TRIANGLE)
 i = 0
 while i < 6:
     mainPile.append(newTile)
     i = i + 1
 
 # create all safe tiles
-newTile = Tile(TileFaction.SEER, TileShape.SQUARE, safe=True)
+newTile = DreamTile(DreamTileFaction.SEER, DreamTileShape.SQUARE, safe=True)
 safePile.append(newTile)
-newTile = Tile(TileFaction.SEER, TileShape.CIRCLE, safe=True)
+newTile = DreamTile(DreamTileFaction.SEER, DreamTileShape.CIRCLE, safe=True)
 safePile.append(newTile)
-newTile = Tile(TileFaction.SEER, TileShape.TRIANGLE, safe=True)
+newTile = DreamTile(DreamTileFaction.SEER, DreamTileShape.TRIANGLE, safe=True)
 safePile.append(newTile)
-newTile = Tile(TileFaction.CHAMELEON, TileShape.SQUARE, safe=True)
+newTile = DreamTile(DreamTileFaction.CHAMELEON, DreamTileShape.SQUARE, safe=True)
 safePile.append(newTile)
-newTile = Tile(TileFaction.CHAMELEON, TileShape.CIRCLE, safe=True)
+newTile = DreamTile(DreamTileFaction.CHAMELEON, DreamTileShape.CIRCLE, safe=True)
 safePile.append(newTile)
-newTile = Tile(TileFaction.CHAMELEON, TileShape.TRIANGLE, safe=True)
+newTile = DreamTile(DreamTileFaction.CHAMELEON, DreamTileShape.TRIANGLE, safe=True)
 safePile.append(newTile)
-newTile = Tile(TileFaction.JUGGLER, TileShape.SQUARE, safe=True)
+newTile = DreamTile(DreamTileFaction.JUGGLER, DreamTileShape.SQUARE, safe=True)
 safePile.append(newTile)
-newTile = Tile(TileFaction.JUGGLER, TileShape.CIRCLE, safe=True)
+newTile = DreamTile(DreamTileFaction.JUGGLER, DreamTileShape.CIRCLE, safe=True)
 safePile.append(newTile)
-newTile = Tile(TileFaction.JUGGLER, TileShape.TRIANGLE, safe=True)
+newTile = DreamTile(DreamTileFaction.JUGGLER, DreamTileShape.TRIANGLE, safe=True)
 safePile.append(newTile)
-newTile = Tile(TileFaction.PYRO, TileShape.SQUARE, safe=True)
+newTile = DreamTile(DreamTileFaction.PYRO, DreamTileShape.SQUARE, safe=True)
 safePile.append(newTile)
-newTile = Tile(TileFaction.PYRO, TileShape.CIRCLE, safe=True)
+newTile = DreamTile(DreamTileFaction.PYRO, DreamTileShape.CIRCLE, safe=True)
 safePile.append(newTile)
-newTile = Tile(TileFaction.PYRO, TileShape.TRIANGLE, safe=True)
+newTile = DreamTile(DreamTileFaction.PYRO, DreamTileShape.TRIANGLE, safe=True)
 safePile.append(newTile)
 
 # shuffle
@@ -163,7 +163,7 @@ while i < 5:
     i = i + 1
 i = 0
 while i < 5:
-    print(mainPile.pop(), end=" ")
+    print(safePile.pop(), end=" ")
     i = i + 1
 
 print()
